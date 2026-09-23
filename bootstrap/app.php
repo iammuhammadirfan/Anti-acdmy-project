55084f7cf05cc3648656e310e21a8cb64c9aa954
unknown <irfanseoexpert420@gmail.com>
2026-09-23 15:38:33 +0500

    Initial Laravel project chnges

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->validateCsrfTokens(except: [
             'api/ai/chat',
         ]);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
