fa78da564b47dbaffa423f57efba434150395964
Muhammad irfan Fayyaz <116436110+iammuhammadirfan@users.noreply.github.com>
2026-09-25 16:19:23 +0500

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
-
+use Illuminate\Support\Facades\Mail;
+use App\Mail\Transport\BrevoApiTransport;
 use Illuminate\Support\Facades\View;
 use App\Models\Setting;
 
